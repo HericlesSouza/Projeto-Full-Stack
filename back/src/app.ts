54a6e5392d9edef190f0c98ca6de88cf8f713fe6
Hericles <hericles_003@hotmail.com>
2023-06-01 12:22:10 -0300

    feat: add cors to back-end

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,13 +1,14 @@
 import "reflect-metadata"
 import "express-async-errors"
+import cors from "cors";
 import express, { Application } from "express"
 import { handleErrors } from "./error"
 import clientRoutes from "./routes/clients.routes"
 import loginRoutes from "./routes/login.routes"
 import contactRoutes from "./routes/contacts.routes"
-
 const app: Application = express()
 
+app.use(cors());
 app.use(express.json())
 
 app.use("/clients", clientRoutes)
